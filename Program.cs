39b36a9f3229dc1e85bc2aa4b064967de7035f58
Dr. Deckto <fynjy138@gmail.com>
2021-12-23 06:38:13 +0300

    реализованно добавление однопалубных и двухпалубных кораблей

--- Program.cs
+++ Program.cs
@@ -4,8 +4,19 @@ namespace Lesson3
 {
     class Program
     {
+        const char SHIP_SYMBOL = 'X';
+        enum Ship
+        {
+            Monohull,
+            DoubleDeck,
+            ThreeDeck, 
+            FourDeck
+        }
+
         static void Main()
         {
+            goto m;
+
             Console.WriteLine("Задание 1");
             {
                 Random rand = new();
@@ -101,11 +112,398 @@ namespace Lesson3
 
             Console.WriteLine();
             Console.WriteLine();
+
+
+            m: //УДАЛИТЬ ПОСЛЕ ЗАВЕРШЕНИЯ РАБОТЫ НАД 4 ЗАДАНИЕМ!!!!!!!!!!!!!!!!!!!
+
+
             Console.WriteLine("Задание 4");
             {
+                char[,] field = new char[10, 10];
 
+                for (int i = 0; i < field.GetLength(0); i++)
+                {
+                    for (int j = 0; j < field.GetLength(1); j++)
+                    {
+                        field[i, j] = 'O';
+                    }
+                }
+
+                int ship1 = 4;
+                int ship2 = 3;
+                int ship3 = 2;
+                int ship4 = 1;
+
+                while (true)
+                {
+                    if (ship1 == 0 && ship2 == 0 && ship3 == 0 && ship4 == 0)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("На данный момент доска выглядит так : ");
+                    DrawField(field);
+
+                    Console.WriteLine($"У вас есть {ship1} однопалубных, {ship2} двухпалубных, {ship3} трехпалубных, {ship4} четырехпалубных");
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Введите 1, чтобы добавить однопалубный корабль, 2 - двухапалубный, 3 - трезпалубный, 4 - черырехпалубный");
+
+                    int n = Convert.ToInt32(Console.ReadLine());
+
+                    switch (n)
+                    {
+                        case 1:
+                            if (ship1 > 0)
+                            { 
+                                ship1--;
+                                AddShipToBoard(field, Ship.Monohull);
+                            }
+                            else
+                            {
+                                Console.WriteLine("У вас закончились однопалубные корабли!");
+                                Console.ReadKey();
+                            }
+                            break;
+
+                        case 2:
+                            if (ship2 > 0)
+                            {
+                                ship2--;
+                                AddShipToBoard(field, Ship.DoubleDeck);
+                            }
+                            else
+                            {
+                                Console.WriteLine("У вас закончились двухпалубные корабли");
+                                Console.ReadKey();
+                            }
+                            break;
+
+                        case 3:
+                            if (ship3 > 0)
+                            {
+                                ship3--;
+                                AddShipToBoard(field, Ship.ThreeDeck);
+                            }
+                            else
+                            {
+                                Console.WriteLine("У вас закончились трехпалуьные корабли");
+                                Console.ReadKey();
+                            }
+                            break;
+
+                        case 4:
+                            if (ship4 > 0)
+                            {
+                                ship4--;
+                                AddShipToBoard(field, Ship.FourDeck);
+                            }
+                            else
+                            {
+                                Console.WriteLine("У вас закончились четырехпалубные корабли");
+                                Console.ReadKey();
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine("Проверьте правильность ввода");
+                            Console.ReadKey();
+                            break;
+                    }
+                    Console.Clear();
+                }
+
+                Console.Clear();
+
+                Console.WriteLine("Конечное расположение кораблей выглядит так :");
+                DrawField(field);
+
+                Console.ReadKey();
             }
         }
 
+        static void AddShipToBoard(char[,] field,Ship ship)
+        {
+            bool isShipNotAdded = true;
+
+            Console.Clear();
+
+            while (isShipNotAdded)
+            {
+                Console.WriteLine("На данный момент доска выглядит так :");
+                DrawField(field);
+
+                Console.WriteLine("Напишите координаты\nP.S. координаты начинаются с 0; 0");
+
+                bool isItNotConverting = true;
+                int coordinateXBeg = 0;
+                int coordinateYBeg = 0;
+                int coordinateXEnd = 0;
+                int coordinateYEnd = 0;
+
+                while (isItNotConverting)
+                {
+                    try
+                    {
+                        if (ship == Ship.Monohull)
+                        {
+                            Console.WriteLine("Введите координату x (номер строки)");
+                            coordinateXBeg = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Введите координату y (номер столбца)");
+                            coordinateYBeg = Convert.ToInt32(Console.ReadLine());
+
+                            if (!(coordinateXBeg >= 0 && coordinateXBeg <= field.GetLength(0) - 1 && coordinateYBeg >= 0 && coordinateYEnd <= field.GetLength(1) - 1))
+                            {
+                                Console.WriteLine("Вы вышли за пределы вашей доски!!!!!");
+                                throw new Exception();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Введите начало корабля по координате x (номер строки)");
+                            coordinateXBeg = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Введите начало корабля по координате y (номер строки)");
+                            coordinateYBeg = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Введите конец корабля по координате x (номер строки)");
+                            coordinateXEnd = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Введите конец корабля по координате y (номер строки)");
+                            coordinateYEnd = Convert.ToInt32(Console.ReadLine());
+
+                            if (!(coordinateXBeg >= 0 && coordinateXBeg <= field.GetLength(0) - 1 && coordinateYBeg >= 0 && coordinateYBeg <= field.GetLength(1) - 1))
+                            {
+                                Console.WriteLine("Вы вышли за пределы вашей доски!!!!!");
+                                throw new Exception();
+                            }
+                            else if (!(coordinateXEnd >= 0 && coordinateXEnd <= field.GetLength(0) - 1 && coordinateYEnd >= 0 && coordinateYEnd <= field.GetLength(1) - 1))
+                            {
+                                Console.WriteLine("Вы вышли за пределы вашей доски!!!!!");
+                                throw new Exception();
+                            }
+
+                        }
+                        isItNotConverting = false;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Ну, с кем не бывает, попробуй еще раз :) ");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+
+                }
+
+                switch (ship)
+                {
+                    case Ship.Monohull:
+                        {
+                            if (IsFieldClear(field, coordinateXBeg, coordinateYBeg, Ship.Monohull))
+                            {
+                                field[coordinateXBeg, coordinateYBeg] = SHIP_SYMBOL;
+                                isShipNotAdded = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Видимо, вы неправильно ввели координаты, либо на том месте уже распологался корабль, пожалуйста, повторите попытку снова.\nДля продолжения нажмите любую кнопку.");
+                                Console.ReadKey();
+                                Console.Clear();
+                            }
+                        }
+                        break;
+
+                    case Ship.DoubleDeck:
+                    case Ship.ThreeDeck:
+                    case Ship.FourDeck:
+                        {
+                            if (IsFieldClear(field, coordinateXBeg, coordinateYBeg, Ship.DoubleDeck, coordinateXEnd, coordinateYEnd))
+                            {
+                                PlacingShipsOnTheBoard(field, coordinateXBeg, coordinateYBeg, coordinateXEnd, coordinateYEnd);
+                                isShipNotAdded = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Видимо, вы неправильно ввели координаты, либо на том месте уже распологался корабль, пожалуйста, повторите попытку снова.\nДля продолжения нажмите любую кнопку.");
+                                Console.ReadKey();
+                                Console.Clear();
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
+        static void DrawField(char[,] field)
+        {
+            for (int i = 0; i< field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Console.Write(field[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool IsFieldClear(char[,] field, int x, int y, Ship ship, int x1 = 0, int y1 = 0)
+        {
+            switch (ship)
+            {
+                case Ship.Monohull:
+                    {
+                        if (field[x, y] == 'O')
+                        {
+                            if (x == 0 && y == 0)
+                            {
+                                if (field[x, y] == 'O' && field[x + 1, y + 1] == 'O' && field[x + 1, y] == 'O')
+                                {
+                                    return true;
+                                }
+                            }
+                            else if (x == field.GetLength(0) - 1 && y == field.GetLength(1) - 1)
+                            {
+                                if (field[x, y] == 'O' && field[x - 1, y - 1] == 'O' && field[x - 1, y] == 'O')
+                                {
+                                    return true;
+                                }
+                            }
+                            else if (x == 0 && y != 0 && y != field.GetLength(1) - 1)
+                            {
+                                if (field[x, y - 1] == 'O' && field[x, y + 1] == 'O' && field[x + 1, y - 1] == 'O' && field[x + 1, y] == 'O' && field[x + 1, y + 1] == 'O')
+                                {
+                                    return true;
+                                }
+                            }
+                            else if (x == field.GetLength(0) - 1 && y != 0 && y != field.GetLength(1) - 1)
+                            {
+                                if (field[x, y - 1] == 'O' && field[x, y + 1] == 'O' && field[x - 1, y - 1] == 'O' && field[x - 1, y] == 'O' && field[x - 1, y + 1] == 'O')
+                                {
+                                    return true;
+                                }
+                            }
+                            else if (x != 0 && y == 0 && x != field.GetLength(0) - 1)
+                            {
+                                if (field[x - 1, y] == 'O' && field[x, y] == 'O' && field[x + 1, y] == 'O' && field[x - 1, y + 1] == 'O' && field[x, y + 1] == 'O' && field[x + 1, y + 1] == 'O')
+                                {
+                                    return true;
+                                }
+                            }
+                            else if (x != 0 && y == field.GetLength(1) - 1 && x != field.GetLength(0) - 1)
+                            {
+                                if (field[x - 1, y] == 'O' && field[x, y] == 'O' && field[x + 1, y] == 'O' && field[x - 1, y - 1] == 'O' && field[x, y - 1] == 'O' && field[x + 1, y - 1] == 'O')
+                                {
+                                    return true;
+                                }
+                            }
+                            else
+                            {
+                                for (int i = x - 1; i <= x + 1; i++)
+                                {
+                                    for (int j = y - 1; j <= y + 1; j++)
+                                    {
+                                        if (field[i,j] == SHIP_SYMBOL)
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                }
+
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+
+                case Ship.DoubleDeck:
+                    {
+                        if ((x == x1 && y1 - y == 1) || (y == y1 && x1 - x == 1))
+                        {
+                            if (x == 0 && y == 0)
+                            {
+                                for (int i = x; i <= x1 + 1; i++)
+                                {
+                                    for (int j = y; j <= y1 + 1; j++)
+                                    {
+                                        if (field[i, j] == SHIP_SYMBOL)
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                }
+                            }
+                            else if (x == 0 && y != 0)
+                            {
+                                for (int i = x; i <= x1 + 1; i++)
+                                {
+                                    for (int j = y - 1; j <= y1 + 1; j++)
+                                    {
+                                        if (field[i,j] == SHIP_SYMBOL)
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                }
+                            }
+                            else if (y == 0  && x != 0)
+                            {
+                                for (int i = x - 1; i <= x1 + 1; i++)
+                                {
+                                    for (int j = y; j <= y1 + 1; j++)
+                                    {
+                                        if (field[i,j] == SHIP_SYMBOL)
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                for (int i = x - 1; i <= x1 + 1; i++)
+                                {
+                                    for (int j = y - 1; j <= y1 + 1; j++)
+                                    {
+                                        if (field[i,j] == SHIP_SYMBOL)
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                }
+                            }
+                            return true;
+                        }
+                    }
+                    return false;
+
+                case Ship.ThreeDeck:
+                    {
+
+                    }
+                    return false;
+
+                case Ship.FourDeck:
+                    {
+
+                    }
+                    return false;
+
+                default:
+                    return false;
+            }
+        }
+
+        static void PlacingShipsOnTheBoard(char[,] field, int x, int y, int x1, int y1)
+        {
+            for (int i = x; i <= x1; i++)
+            {
+                for (int j = y; j <= y1; j++)
+                {
+                    field[i, j] = 'X';
+                }
+            }
+        }
     }
 }
